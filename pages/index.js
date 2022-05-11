6c137a03cc4ad0eb4e5116e7b1fe623d3e706962
JorgeIturrieta <jorge.10.iturrieta@gmail.com>
2022-05-11 12:43:05 -0300

    some text change

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, my name is Jorge Iturrieta a Web devoloper from Argentina!</p>
+        <p>Hi, my name is Jorge Iturrieta a Web devoloper from Argentina! text changes</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
